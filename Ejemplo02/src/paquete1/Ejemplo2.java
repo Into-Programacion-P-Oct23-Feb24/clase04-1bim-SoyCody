ede0c315c66e6011e5778059a0ee8de7c130fb98
unknown <codycabrerraa@gmail.com>
2023-11-01 16:54:21 -0500

    Notas

--- Ejemplo02/src/paquete1/Ejemplo2.java
+++ Ejemplo02/src/paquete1/Ejemplo2.java
@@ -31,6 +31,7 @@ public class Ejemplo2 {
         notaAsignatura1 = entrada.nextDouble();
         System.out.println("Ingrese nombre de asignatura 2");
         entrada.nextLine(); // limpieza de buffer
+        //Nos permite hacer una limpieza para una nueva cadena
         nombreAsignatura2 = entrada.nextLine();
         System.out.println("Ingrese nota de asignatura 2");
         notaAsignatura2 = entrada.nextDouble();
